ba877d1aff4f21a64ae8c4b9247001f82c214a02
marcos-bah <marcos.barbosa@unifei.edu.br>
2021-06-08 21:58:32 -0300

    [ADD] first commit

---
+++ bin/server_deficientes_visuais_proex.dart
@@ -0,0 +1,5 @@
+import 'package:server_deficientes_visuais_proex/server_deficientes_visuais_proex.dart' as server_deficientes_visuais_proex;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_deficientes_visuais_proex.calculate()}!');
+}
---
+++ test/server_deficientes_visuais_proex_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_deficientes_visuais_proex/server_deficientes_visuais_proex.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 49);
+  });
+}
