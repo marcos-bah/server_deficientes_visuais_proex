e16b9696eb0ac9e03a5eae86c4850cd0d41c9ede
ygor-salles <ygorsallesaniceto@gmail.com>
2021-08-04 22:56:14 -0300

    Protegendo rota de usuário super

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { UserController } from "./controllers/UserController";
+import { ensureSuper } from "./middlewares/ensureSuper";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
 
 const userController = new UserController();
 const authenticateUserController = new AuthenticateUserController();
 
-router.post('/users', userController.create);
-router.get('/users', userController.ready);
-router.get('/users/:id', userController.readyById);
-router.put('/users/:id', userController.update);
-router.delete('/users/:id', userController.delete);
+router.post('/users', ensureAuthenticated, ensureSuper, userController.create);
+router.get('/users', ensureAuthenticated, userController.ready);
+router.get('/users/:id', ensureAuthenticated, userController.readyById);
+router.put('/users/:id', ensureAuthenticated, ensureSuper, userController.update);
+router.delete('/users/:id', ensureAuthenticated, ensureSuper, userController.delete);
 
 router.post('/login', authenticateUserController.handle);
 
